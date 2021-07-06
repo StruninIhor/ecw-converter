738d3100dcab30173fe6edcb425f1a41231b9a54
Strunin Ihor <strunin.ihor@gmail.com>
2021-07-06 15:47:44 +0500

    changed zip to bz2

--- ecw_converter/ecw_to_cog.sh
+++ ecw_converter/ecw_to_cog.sh
@@ -4,11 +4,11 @@
 
 mkdir logs
 
-mkdir zip && mv *.zip zip && cd zip
+mkdir bz2 && mv *.bz2 bz2 && cd bz2
 echo -e "\nUnzipping files"
-ls *.zip | xargs -n1 '-I{}' sh -c 'echo {}; lbunzip2 -k {}' &> ../logs/unzip.log
+ls *.bz2 | xargs -n1 '-I{}' sh -c 'echo {}; lbunzip2 -k {}' &> ../logs/unzip.log
 
-cd .. && mkdir ecw && mv zip/*.ecw ecw && mkdir tif
+cd .. && mkdir ecw && mv bz2/*.ecw ecw && mkdir tif
 echo -e "\nGenerating Geotiff & COG files using ecw_convert_2_cog.py"
 ecw_convert_2_cog.py ecw tif ? &> logs/ecw_convert_2_cog.log
 
